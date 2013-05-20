3bc997abe80495abacf397576172afa21d56614a
Barend vd Heever <benstof@gmail.com>
2013-05-20 14:46:19 -0400

    Alles

--- Unit2.dfm
+++ Unit2.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 416
-  ClientWidth = 639
+  ClientHeight = 511
+  ClientWidth = 762
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -14,27 +14,320 @@ object Form1: TForm1
   OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
-  object image: TImage32
+  object dxRibbonStatusBar1: TdxRibbonStatusBar
     Left = 0
-    Top = 64
-    Width = 639
-    Height = 352
-    Align = alBottom
-    Bitmap.ResamplerClassName = 'TNearestResampler'
-    BitmapAlign = baTopLeft
-    Scale = 1.000000000000000000
-    ScaleMode = smNormal
-    TabOrder = 0
-    OnMouseDown = imageMouseDown
-    OnMouseMove = imageMouseMove
+    Top = 488
+    Width = 762
+    Height = 23
+    Panels = <>
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clDefault
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ExplicitWidth = 724
   end
-  object Button1: TButton
+  object dxRibbon1: TdxRibbon
+    Left = 0
+    Top = 0
+    Width = 762
+    Height = 117
+    BarManager = dxBarManager1
+    ColorSchemeName = 'Blue'
+    Style = rs2010
+    Contexts = <>
+    TabOrder = 3
+    TabStop = False
+    ExplicitWidth = 724
+    object dxRibbon1Tab1: TdxRibbonTab
+      Active = True
+      Caption = 'Irrigation'
+      Groups = <
+        item
+          ToolbarName = 'dxBarManager1Bar1'
+        end
+        item
+          ToolbarName = 'dxBarManager1Bar2'
+        end
+        item
+          ToolbarName = 'dxBarManager1Bar3'
+        end>
+      Index = 0
+    end
+    object dxRibbon1Tab2: TdxRibbonTab
+      Caption = 'Cad'
+      Groups = <
+        item
+          ToolbarName = 'dxBarManager1Bar1'
+        end
+        item
+        end
+        item
+        end>
+      Index = 1
+    end
+    object dxRibbon1Tab3: TdxRibbonTab
+      Caption = 'Dtm'
+      Groups = <>
+      Index = 2
+    end
+  end
+  object dxDockSite1: TdxDockSite
+    Left = 0
+    Top = 117
+    Width = 762
+    Height = 371
+    Align = alClient
+    ExplicitLeft = 55
+    ExplicitTop = 123
+    ExplicitWidth = 666
+    ExplicitHeight = 359
+    DockingType = 5
+    OriginalWidth = 762
+    OriginalHeight = 371
+    object dxLayoutDockSite1: TdxLayoutDockSite
+      Left = 0
+      Top = 0
+      Width = 762
+      Height = 371
+      DockingType = 0
+      OriginalWidth = 300
+      OriginalHeight = 200
+    end
+    object dxTabContainerDockSite1: TdxTabContainerDockSite
+      Left = 0
+      Top = 0
+      Width = 762
+      Height = 371
+      ActiveChildIndex = 0
+      AllowFloating = True
+      AutoHide = False
+      DockingType = 0
+      OriginalWidth = 441
+      OriginalHeight = 140
+      object dxDockPanel2: TdxDockPanel
+        Left = 0
+        Top = 0
+        Width = 758
+        Height = 323
+        AllowFloating = True
+        AutoHide = False
+        Caption = 'dxDockPanel2'
+        ExplicitWidth = 441
+        ExplicitHeight = 116
+        DockingType = 0
+        OriginalWidth = 441
+        OriginalHeight = 140
+        object browser: TWebBrowser
+          Left = 98
+          Top = 0
+          Width = 660
+          Height = 323
+          Align = alClient
+          TabOrder = 0
+          ExplicitLeft = 104
+          ExplicitWidth = 654
+          ControlData = {
+            4C000000DF3F00002D2100000000000000000000000000000000000000000000
+            000000004C000000000000000000000001000000E0D057007335CF11AE690800
+            2B2E126208000000000000004C0000000114020000000000C000000000000046
+            8000000000000000000000000000000000000000000000000000000000000000
+            00000000000000000100000000000000000000000000000000000000}
+        end
+        object Panel1: TPanel
+          Left = 0
+          Top = 0
+          Width = 98
+          Height = 323
+          Align = alLeft
+          TabOrder = 1
+          object Button5: TButton
+            Left = 8
+            Top = 64
+            Width = 75
+            Height = 65
+            Caption = 'New Report'
+            TabOrder = 0
+            OnClick = Button5Click
+          end
+        end
+      end
+      object dxDockPanel1: TdxDockPanel
+        Left = 0
+        Top = 0
+        Width = 758
+        Height = 323
+        AllowFloating = True
+        AutoHide = False
+        Caption = 'dxDockPanel1'
+        ExplicitWidth = 441
+        ExplicitHeight = 116
+        DockingType = 0
+        OriginalWidth = 441
+        OriginalHeight = 140
+        object image: TImage32
+          Left = 8
+          Top = 48
+          Width = 105
+          Height = 147
+          Bitmap.ResamplerClassName = 'TNearestResampler'
+          BitmapAlign = baTopLeft
+          Scale = 1.000000000000000000
+          ScaleMode = smNormal
+          TabOrder = 0
+          OnMouseDown = imageMouseDown
+          OnMouseMove = imageMouseMove
+        end
+        object Button1: TButton
+          Left = 127
+          Top = 104
+          Width = 75
+          Height = 25
+          Caption = 'Button1'
+          TabOrder = 1
+          OnClick = Button1Click
+        end
+        object Button2: TButton
+          Left = 240
+          Top = 104
+          Width = 75
+          Height = 25
+          Caption = 'Button2'
+          TabOrder = 2
+          OnClick = Button2Click
+        end
+        object Button3: TButton
+          Left = 352
+          Top = 104
+          Width = 75
+          Height = 25
+          Caption = 'Button3'
+          TabOrder = 3
+          OnClick = Button3Click
+        end
+        object Button4: TButton
+          Left = 363
+          Top = 104
+          Width = 75
+          Height = 25
+          Caption = 'Button4'
+          TabOrder = 4
+        end
+      end
+    end
+  end
+  object OpenDialog1: TOpenDialog
+    Left = 544
+    Top = 48
+  end
+  object OpenPictureDialog: TOpenPictureDialog
+    Left = 608
+    Top = 48
+  end
+  object dxBarManager1: TdxBarManager
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    Categories.Strings = (
+      'Default')
+    Categories.ItemsVisibles = (
+      2)
+    Categories.Visibles = (
+      True)
+    PopupMenuLinks = <>
+    UseSystemFont = True
     Left = 8
-    Top = 16
-    Width = 75
-    Height = 25
-    Caption = 'Button1'
-    TabOrder = 1
-    OnClick = Button1Click
+    Top = 136
+    DockControlHeights = (
+      0
+      0
+      0
+      0)
+    object dxBarManager1Bar1: TdxBar
+      Caption = 'Nodes'
+      CaptionButtons = <>
+      DockedLeft = 0
+      DockedTop = 0
+      FloatLeft = 750
+      FloatTop = 8
+      FloatClientWidth = 0
+      FloatClientHeight = 0
+      ItemLinks = <
+        item
+          Visible = True
+          ItemName = 'dxBarButton2'
+        end
+        item
+          Visible = True
+          ItemName = 'dxBarButton1'
+        end>
+      OneOnRow = False
+      Row = 0
+      UseOwnFont = False
+      Visible = True
+      WholeRow = False
+    end
+    object dxBarManager1Bar2: TdxBar
+      Caption = 'Pipes'
+      CaptionButtons = <>
+      DockedLeft = 48
+      DockedTop = 0
+      FloatLeft = 750
+      FloatTop = 8
+      FloatClientWidth = 0
+      FloatClientHeight = 0
+      ItemLinks = <
+        item
+          Visible = True
+          ItemName = 'dxBarButton2'
+        end
+        item
+          Visible = True
+          ItemName = 'dxBarButton1'
+        end>
+      OneOnRow = False
+      Row = 0
+      UseOwnFont = False
+      Visible = True
+      WholeRow = False
+    end
+    object dxBarManager1Bar3: TdxBar
+      Caption = 'Pivots'
+      CaptionButtons = <>
+      DockedLeft = 96
+      DockedTop = 0
+      FloatLeft = 750
+      FloatTop = 8
+      FloatClientWidth = 0
+      FloatClientHeight = 0
+      ItemLinks = <
+        item
+          Visible = True
+          ItemName = 'dxBarButton2'
+        end
+        item
+          Visible = True
+          ItemName = 'dxBarButton1'
+        end>
+      OneOnRow = False
+      Row = 0
+      UseOwnFont = False
+      Visible = True
+      WholeRow = False
+    end
+    object dxBarButton1: TdxBarButton
+      Caption = 'Add'
+      Category = 0
+      Hint = 'Add'
+      Visible = ivAlways
+    end
+    object dxBarButton2: TdxBarButton
+      Caption = 'Delete'
+      Category = 0
+      Hint = 'Delete'
+      Visible = ivAlways
+    end
   end
 end
--- Unit2.pas
+++ Unit2.pas
@@ -5,10 +5,62 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
-  gr32, GR32_Polygons, gr32_layers, GR32_Image, Vcl.StdCtrls;
+  gr32, GR32_Polygons, gr32_layers, GR32_Image,
+  gr32_resamplers, Vcl.StdCtrls, Vcl.ExtDlgs, cxGraphics, cxControls,
+  cxLookAndFeels, cxLookAndFeelPainters, dxRibbonSkins, cxClasses, dxRibbon,
+  dxBar, dxRibbonGallery, dxRibbonBackstageView, dxRibbonMiniToolbar,
+  dxStatusBar, dxRibbonStatusBar, Vcl.ButtonGroup, Vcl.ToolWin, Vcl.ActnMan,
+  Vcl.ActnCtrls, MSHTML,Vcl.Ribbon, ActiveX, Vcl.RibbonLunaStyleActnCtrls, Vcl.OleCtrls, SHDocVw,
+  cxPC, dxDockControl, dxDockPanel, Vcl.ExtCtrls;
+
+
+const
+
+HTMLStr: AnsiString =
+'<html> '+
+'<head> '+
+'</head> '+
+''+
+'<body  scroll=no onload="" style="color:#000; font-family:arial; background-color:#fff; padding:0px; margin:0px; border:0; "> '+
+
+'<table border="0" bordercolor="#fff" style="background-color:#FFF" width="100%" cellpadding="3" cellspacing="3">      '+
+'	<tr>' +
+'		<td><b>Irrigation Filename:<b></td>  '+
+'	</tr>                              '+
+'	<tr>                                 '+
+'		<td><b>List of Nodes & Pipes</b></td>                '+
+'	</tr>                                 '+
+'	<tr>                                   '+
+'		<td><b>Date</b>: 20/5/2013    <b>Time:</b> 14:13</td>                   '+
+'	</tr>                                    '+
+'	<tr>                                     '+
+'		<td>Materials for BLOCK # 0</td>                    '+
+'	</tr>                                    '+
+'	<tr>                                     '+
+'		<td>Please note, information regarding the block valves are shown in the mainline'+
+ 'and also repeated in the individual blocks.</td>                    '+
+'	</tr>                                    '+
+'	<tr>                                     '+
+'		<td><b>Summary of total nodes</b> </td>                    '+
+'	</tr>                                    '+
+'	<tr>                                     '+
+'		<td>HYDRO PC 16MM X 2.2L/H X 0.6M Nozzle     #       8</td>                     '+
+'	</tr>                                    '+
+'	<tr>                                     '+
+'		<td><b>Detail of node sizes with OD pipe sizes</b></td>                    '+
+'	</tr>                                    '+
+'	<tr>                                     '+
+'		<td>HYDRO PC 16MM X 2.2L/H X 0.6M Nozzle     #      8 > </td>                    '+
+'	</tr>                                    '+
+'	<tr>                                     '+
+'		<td>PIPES                                          OD           Total length m</td>                    '+
+'	</tr>                                    '+
+'</table> '+
+
+'</body> '+
+'</html> ';
 
 type
-
   coords = record
     x,y : single;
   end;
@@ -18,8 +70,32 @@ type
   end;
 
   TForm1 = class(TForm)
+    OpenDialog1: TOpenDialog;
+    OpenPictureDialog: TOpenPictureDialog;
+    dxRibbonStatusBar1: TdxRibbonStatusBar;
+    dxBarManager1: TdxBarManager;
+    dxBarManager1Bar1: TdxBar;
+    dxBarButton1: TdxBarButton;
+    dxRibbon1: TdxRibbon;
+    dxRibbon1Tab1: TdxRibbonTab;
+    dxRibbon1Tab2: TdxRibbonTab;
+    dxRibbon1Tab3: TdxRibbonTab;
+    dxBarManager1Bar2: TdxBar;
+    dxBarManager1Bar3: TdxBar;
+    dxBarButton2: TdxBarButton;
+    dxDockSite1: TdxDockSite;
+    dxDockPanel1: TdxDockPanel;
+    dxDockPanel2: TdxDockPanel;
+    dxTabContainerDockSite1: TdxTabContainerDockSite;
+    browser: TWebBrowser;
     image: TImage32;
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    dxLayoutDockSite1: TdxLayoutDockSite;
+    Panel1: TPanel;
+    Button5: TButton;
     procedure FormCreate(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure PaintSimpleDrawingHandler(Sender: TObject; Buffer: TBitmap32);
@@ -28,6 +104,8 @@ type
     procedure imageMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer; Layer: TCustomLayer);
     procedure Button1Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
   private
     { Private declarations }
     Polygon1: TPolygon32;
@@ -39,6 +117,7 @@ type
     { Public declarations }
     x,y : integer;
     from_x, from_y, to_x, to_y : single;
+      I: TBitmapLayer;
 
     property Selection: TPositionedLayer read FSelection write SetSelection;
   end;
@@ -47,6 +126,7 @@ var
   Form1: TForm1;
   nl : Line;
   rand : array[0..10000] of tpoint;
+  HTMLWindow2: IHTMLWindow2;
 
 implementation
 
@@ -78,33 +158,81 @@ var
   W, H: Integer;
 begin
   { deselect everything }
-  Selection := nil;
+ { Selection := nil;
   W := image.Bitmap.Width;
   H := image.Bitmap.Height;
 
-  { Create a new bitmap to store a flattened image }
   B := TBitmap32.Create;
   try
     B.SetSize(W, H);
     image.PaintTo(B, Rect(0, 0, W, H));
 
-    { destroy all the layers of the original image... }
     image.Layers.Delete(0);//.Clear;
-    //RBLayer := nil; // note that RBLayer reference is destroyed here as well.
-                    // The rubber band will be recreated during the next
-                    // SetSelection call. Alternatively, you can delete
-                    // all the layers except the rubber band.
-
-    { ...and overwrite it with the flattened one }
     image.Bitmap := B;
   finally
     B.Free;
-  end;
+  end;  }
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
-var i, j : integer;
+var
+  P: TPoint;
+  W, H: Single;
 begin
+
+  {with OpenPictureDialog do
+    if Execute then
+    begin  }
+      I := TBitmapLayer.Create(image.Layers);
+      with I do
+      try
+        Bitmap.LoadFromFile('sunset.jpg');
+        Bitmap.DrawMode := dmBlend;
+
+        with image.GetViewportRect do
+          P := image.ControlToBitmap(GR32.Point((Right + Left) div 2, (Top + Bottom) div 2));
+
+        W := Bitmap.Width * 0.5;
+        H := Bitmap.Height * 0.5;
+
+      //  with image.Bitmap do
+          I.Location := GR32.FloatRect(P.X - W, P.Y - H, P.X + W, P.Y + H);
+
+        Scaled := True;
+
+        //OnMouseDown := LayerMouseDown;
+      except
+        Free;
+        raise;
+      end;
+      Selection := B;
+
+    //end;
+
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  L: TFloatRect;
+  b : TBitmap32;
+begin
+
+   image.SetupBitmap(true, clwhite32);
+   b := image.Bitmap;
+
+   b.Clear($FFFFFFFF);
+   b.DrawMode := dmOpaque;
+   b.Canvas.Font.Name := 'verdana';
+   b.Canvas.Font.Color := $00000000;
+   b.Canvas.Font.Size := 30;
+   b.Canvas.Textout(100,100, 'kllkjljl');
+
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+
+   //opopo
 
 end;
 
@@ -189,9 +317,36 @@ p1, p2 : TFixedPoint;
 P: TPoint;
 rn, rnn,i : integer;
 
+var
+   aStream     : TMemoryStream;
 begin
 
-      for I := 0 to 9999 do
+
+  browser.Navigate('about:blank');
+    if Assigned(browser.Document) then
+    begin
+      aStream := TMemoryStream.Create;
+      try
+         aStream.WriteBuffer(Pointer(HTMLStr)^, Length(HTMLStr));
+         //aStream.Write(HTMLStr[1], Length(HTMLStr));
+         aStream.Seek(0, soFromBeginning);
+         (browser.Document as IPersistStreamInit).Load(TStreamAdapter.Create(aStream));
+          //maps_browser.OleObject.Document.Body.Style.OverflowX := 'hidden';
+
+      finally
+         aStream.Free;
+      end;
+      HTMLWindow2 := (browser.Document as IHTMLDocument2).parentWindow;
+    end;
+
+   //BarButtonItem bAdd = new BarButtonItem();
+   dxBarButton1.Name := 'bStuff';
+  // dxBarButton1.Content = "stuff";
+  // dxBarButton1.Glyph = bmp;
+
+  // RibbonGroup1.Items.Add;
+
+ {     for I := 0 to 9999 do
       begin
          rn := Random(800);
          rnn := Random(800);
@@ -230,7 +385,11 @@ begin
 
   L.OnPaint := PaintSimpleDrawingHandler;
   L.Tag := 1;
-  //Selection := L;
+  //Selection := L;  }
+
+
+
+
 
 
 
@@ -242,14 +401,14 @@ procedure TForm1.imageMouseDown(Sender: TObject; Button: TMouseButton;
   var len : integer;
 begin
 
-   len := length(nl.points);
+ {  len := length(nl.points);
 
    setlength(nl.points, len+1);
    nl.points[len].x := x;
    nl.points[len].y := y;
 
    nl.points[0].x := x;
-   nl.points[0].y := y;
+   nl.points[0].y := y; }
 
 
 end;
@@ -258,10 +417,11 @@ procedure TForm1.imageMouseMove(Sender: TObject; Shift: TShiftState; X,
   Y: Integer; Layer: TCustomLayer);
 begin
 
-     L.Location := FloatRect(x - 32,y -32,x+32,y+32);
+    { L.Location := FloatRect(x - 32,y -32,x+32,y+32);
 
      from_x:=x;
-     from_y:=y;
+     from_y:=y;  }
+
 
 end;
 
